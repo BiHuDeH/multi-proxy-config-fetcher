411b7d784e019aae23f020c69cea0294f7f3d1d2
BiHuDeH <172217688+BiHuDeH@users.noreply.github.com>
2025-06-30 22:51:43 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -5,20 +5,20 @@
 SOURCE_URLS = [
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
     "https://t.me/s/v2rayfree",
-    "https://t.me/s/FreeV2rays",
-    "https://t.me/s/v2ray_free_conf",
-    "https://t.me/s/PrivateVPNs",
-    "https://t.me/s/IP_CF_Config",
+    #"https://t.me/s/FreeV2rays",
+    #"https://t.me/s/v2ray_free_conf",
+    #"https://t.me/s/PrivateVPNs",
+    #"https://t.me/s/IP_CF_Config",
     # "https://t.me/s/shadowproxy66",
     # "https://t.me/s/OutlineReleasedKey",
-    "https://t.me/s/prrofile_purple",
-    "https://t.me/s/proxy_shadosocks",
-    "https://t.me/s/meli_proxyy",
+    # "https://t.me/s/prrofile_purple",
+    # "https://t.me/s/proxy_shadosocks",
+    #"https://t.me/s/meli_proxyy",
     "https://t.me/s/DirectVPN",
-    "https://t.me/s/VmessProtocol",
+    # "https://t.me/s/VmessProtocol",
     # "https://t.me/s/ViProxys",
-    "https://t.me/s/heyatserver",
-    "https://t.me/s/vpnfail_vless",
+    # "https://t.me/s/heyatserver",
+    # "https://t.me/s/vpnfail_vless",
     "https://t.me/s/DailyV2RY",
     # Add more URLs here if you want to include additional sources.
 ]
@@ -29,18 +29,18 @@ USE_MAXIMUM_POWER = False
 
 # Desired number of configurations to fetch.
 # This is used only if USE_MAXIMUM_POWER is False.
-SPECIFIC_CONFIG_COUNT = 100
+SPECIFIC_CONFIG_COUNT = 50
 
 # Dictionary of protocols to enable or disable.
 # Set each protocol to True to enable, False to disable.
 ENABLED_PROTOCOLS = {
-    "wireguard://": False,
+    # "wireguard://": False,
     "hysteria2://": True,
     "vless://": True,
     "vmess://": True,
-    "ss://": True,
+    # "ss://": True,
     "trojan://": True,
-    "tuic://": True,
+    # "tuic://": True,
 }
 
 # Maximum age of configurations in days.
